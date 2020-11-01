c061f454e1848acf7a5d3c85f64a6e9dc4050db3
dge07 <73477266+dge07@users.noreply.github.com>
2020-11-01 18:25:15 +0100

    Delete Prostokaty.java

--- Prostokaty.java
+++
@@ -1,9 +0,0 @@
-import java.io.Console;
-
-public class Prostokaty {
-    public static double MojaFunkcja(double x)
-    {
-        return (0.3*x + 0.2)/(1.4-Math.cos(Math.pow(x,2) + 0.3));
-    }
-}
-
