fbbdfe66124df2f6c7077964eaf6ff458aa40bd3
dge07 <73477266+dge07@users.noreply.github.com>
2020-11-01 18:24:53 +0100

    Delete Trapezy.java

--- Trapezy.java
+++
@@ -1,9 +0,0 @@
-public class Trapezy {
-    public static double MojaFunkcja(double x) {
-        return (0.3*x + 0.2)/(1.4-Math.cos(Math.pow(x,2) + 0.3));
-    }
-    public static double st(double x, double y, double n, double z) {
-        return x + (z / n) * (y - x);
-    }
-
-}
